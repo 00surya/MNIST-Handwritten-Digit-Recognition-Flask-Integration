9b011d3aa98fc78d952bc9f9899e3e9e8e7820e3
Surya Verma <72555711+00surya@users.noreply.github.com>
2021-11-24 18:38:00 +0530

    Update api.py

--- services/api.py
+++ services/api.py
@@ -1,21 +1,21 @@
-from keras.models import load_model
+# from keras.models import load_model
 import numpy as np
 import joblib
 import matplotlib.pylab as plt
 import pickle
 
-CNN_ = load_model('./services/CNN_digit_rec.h5')
+# CNN_ = load_model('./services/CNN_digit_rec.h5')
 LR_ = joblib.load('./services/lr_digit_rec.pkl')
 MNB_ = joblib.load('./services/mnb_digit_rec.pkl')
 # KNN_ = joblib.load('./services/knn_digit_rec.pkl')
 # KNN_ = joblib.load('./services/knn_digit_rec.pkl')
 
-def CNN(arr):
+# def CNN(arr):
 
-    arr = arr.reshape(1,28,28,1)
-    c_pred = CNN_.predict([arr])
-    c_pred = np.argmax(c_pred)
-    return c_pred
+#     arr = arr.reshape(1,28,28,1)
+#     c_pred = CNN_.predict([arr])
+#     c_pred = np.argmax(c_pred)
+#     return c_pred
 
 # def KNN(arr):
 #     pred = KNN_.predict([arr])
