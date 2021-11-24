d2d51d37ba20d72e0b154da77576ffd5577dbb78
Surya Verma <72555711+00surya@users.noreply.github.com>
2021-11-24 18:49:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask,render_template,redirect,jsonify
 from flask.globals import request
-from services import api
+# from services import api
 import numpy as np
+import joblib
+import pickle
+
+# CNN_ = load_model('./services/CNN_digit_rec.h5')
+
+
+
+class api:
+    
+    def l_r(self,arr):
+
+        LR_ = joblib.load('./services/lr_digit_rec.pkl')
+        pred = L_R.predict([arr])
+        return pred
+
 
 app = Flask(__name__)
+
+
 
 
 @app.route('/')
@@ -19,10 +36,10 @@ def digit():
 #         cnn_pred = str(api.CNN(arr))
         # knn_pred = api.KNN(arr)
         lr_pred = str(api.lr(arr))
-        mnb_pred = str(api.mnb(arr))
+#         mnb_pred = str(api.mnb(arr))
         # return str(pred)
 #         return jsonify(mnb_res=mnb_pred,l_r_res=lr_pred,cnn_res=cnn_pred)
-        return jsonify(mnb_res=mnb_pred,l_r_res=lr_pred)
+        return jsonify(l_r_res=lr_pred)
         # return(str(cnn_pred))
     return render_template("index.html")
 
