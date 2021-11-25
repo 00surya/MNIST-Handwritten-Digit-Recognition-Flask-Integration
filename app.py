40b7673ad1e403b7e7807d8600b0d6776d9d7a9c
Surya Verma <72555711+00surya@users.noreply.github.com>
2021-11-25 19:03:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ class api:
         
         return pred
     
+    def mnb(self,arr):
+        mnb_ = joblib.load('mnb_digit_rec.pkl')
+        pred = mnb_.predict([arr])
+
+        return pred[0]
+
 # def l_r(arr):
 
 #     L_R = joblib.load('lr_digit_rec.pkl')
@@ -30,13 +36,16 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+
 @app.route('/digit',methods=['POST'])
 def digit():
     if request.method == 'POST':
         arr = request.json['array']
         arr = np.array(arr)
         lr_pred = api().l_r(arr)
-        return jsonify(l_r_res=str(lr_pred[0]))
+        mnb_pred = api().mnb(arr)
+        
+        return jsonify(l_r_res=str(lr_pred[0]),mnb_res=str(mnb_pred))
 
     return render_template("index.html")
 
